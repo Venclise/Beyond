801cd0caf24f4584af95ca8db7f28b4d14d610c8
Venclise <zunnurainzahoor955@gmail.com>
2026-04-22 15:13:16 +0500

    Added a skeleton

--- app/products/item/[productid]/loading.tsx
+++ app/products/item/[productid]/loading.tsx
@@ -3,14 +3,16 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
- <div className="w-full   h-max   mt-24  flex flex-col gap-12  overflow-hidden ">
+    <div className="h-max w-ffull flex flex-col lg:p-10 lg:flex-row items-center justify-between w-full gap-4 lg:gap-10 ">
       {/* Image */}
-        <div className="w-full h-screen " >
-      <Skeleton className=" h-full w-full" />
+           <div className="w-full h-[30rem] lg:w-[50%] lg:h-[35rem]  ">
+
+
+      <Skeleton className=" h-full w-full rounded-xl" />
       </div>
 
     
-    <div className={`flex  justify-between   flex-col lg:flex-row w-full lg:p-10 p-5  `}> 
+      <div className="flex flex-col lg:w-[50%] gap-4 w-full">
         <Skeleton className="h-6 w-3/4" />
         <Skeleton className="h-4 w-1/2" />
         <Skeleton className="h-20 w-full" />
