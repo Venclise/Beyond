2a5df62ab82a85c74d7407d7da0cca03b5cef451
Venclise <zunnurainzahoor955@gmail.com>
2026-05-16 12:05:22 +0500

    Minor change

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,26 @@ const nextConfig = {
     ],
   },
 
- 
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "beyond-pi-snowy.vercel.app" }],
+        destination: "https://www.beyondinteriordesign.pk/:path*",
+        permanent: true,
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "beyond-pi-snowy.vercel.app" }],
+        headers: [{ key: "X-Robots-Tag", value: "noindex" }],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
